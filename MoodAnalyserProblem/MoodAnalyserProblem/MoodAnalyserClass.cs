9c44d7bfe10ab4991e5f2873f9442199363bb399
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-13 18:11:43 +0530

    [Praveen] Add: Using analyse mood method to inspect mood on basis of message recieved

---
+++ MoodAnalyserProblem/MoodAnalyserProblem/MoodAnalyserClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserProblem
+{
+    class MoodAnalyserClass
+    {
+        public static string message;
+
+        /// <summary>
+        /// Analyses the mood.
+        /// </summary>
+        /// <param name="messageRecieved">The message recieved.</param>
+        /// <returns></returns>
+        public string analyseMood(string messageRecieved)
+        {
+            message = messageRecieved.ToUpper();
+            if (message.Contains("SAD"))
+                return "SAD";
+            else if (message.Contains("HAPPY"))
+                return "HAPPY";
+            else
+                return "NO COMMENTS";
+        }
+    }
+}
---
+++ MoodAnalyserProblem/MoodAnalyserProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MoodAnalyserProblem
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            MoodAnalyserClass moodAnalyser = new MoodAnalyserClass();
+            Console.WriteLine("The mood of your customer is {0}", moodAnalyser.analyseMood("I am sad today"));
+            Console.WriteLine("The mood of your customer is {0}", moodAnalyser.analyseMood("I am happy today"));
+            Console.ReadKey();
+        }
+    }
+}
